407f36223f193f529a902b964be27ce4b8bcd421
Abul Hashem Jr <abul@limosys.com>
2020-01-07 21:25:33 -0500

    Dependency setup

---
+++ app/src/main/java/com/project/gamersgeek/GamersGeekMainActivity.kt
@@ -0,0 +1,12 @@
+package com.project.gamersgeek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GamersGeekMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
