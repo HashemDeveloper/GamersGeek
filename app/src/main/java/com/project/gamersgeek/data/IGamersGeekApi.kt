f83214d2ecfc3b5b0aa7ba25912035ce3af4f5c0
Abul Hashem Jr <abul@limosys.com>
2020-01-09 21:09:42 -0500

    Fetches game by its ID

--- app/src/main/java/com/project/gamersgeek/data/IGamersGeekApi.kt
+++ app/src/main/java/com/project/gamersgeek/data/IGamersGeekApi.kt
@@ -1,10 +1,13 @@
 package com.project.gamersgeek.data
 
 import com.project.gamersgeek.models.games.GameListRes
+import com.project.gamersgeek.models.games.GamesRes
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface IGamersGeekApi {
     @GET("/games")
     suspend fun fetchAllGames(): GameListRes
+    @GET("/games/{id}")
+    suspend fun fetchGameById(@Path("id") id: Int): GamesRes
 }
