b3f3166a1e08c3f0def4d7ba33400ecf2731cb1f
Abul Hashem Jr <abul@limosys.com>
2020-02-28 17:38:12 -0500

    Adds video on the recyclerview

--- app/src/main/java/com/project/gamersgeek/views/AllGamesPage.kt
+++ app/src/main/java/com/project/gamersgeek/views/AllGamesPage.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.project.gamersgeek.R
 import com.project.gamersgeek.di.Injectable
@@ -15,6 +16,7 @@ import com.project.gamersgeek.di.viewmodel.ViewModelFactory
 import com.project.gamersgeek.models.games.GameListRes
 import com.project.gamersgeek.utils.ResultHandler
 import com.project.gamersgeek.viewmodels.AllGamesPageViewModel
+import com.project.gamersgeek.views.recycler.AllGameResultAdapter
 import com.project.gamersgeek.views.widgets.GlobalLoadingBar
 import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.fragment_all_games_page.*
@@ -43,8 +45,28 @@ class AllGamesPage: Fragment(), Injectable {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupVideoRecyclerView()
+    }
+
+    private fun setupVideoRecyclerView() {
+        val allGameAdapter = AllGameResultAdapter()
+        all_game_recycler_view_id.layoutManager = LinearLayoutManager(this.context)
+        all_game_recycler_view_id.setActivity(activity)
+        all_game_recycler_view_id.setPlayOnlyFirstVideo(true)
+        all_game_recycler_view_id.setCheckForMp4(false)
+        all_game_recycler_view_id.setVisiblePercent(50f)
+        all_game_recycler_view_id.adapter = allGameAdapter
+        all_game_recycler_view_id.smoothScrollBy(0, 1)
+        all_game_recycler_view_id.smoothScrollBy(0, -1)
         this.allGamesPageViewModel.gameResultLiveData.observe(viewLifecycleOwner, Observer {
-            Timber.d("List: $it")
+            if (it != null && it.size != 0) {
+                allGameAdapter.submitList(it)
+            }
         })
+    }
+
+    override fun onStop() {
+        super.onStop()
+        all_game_recycler_view_id?.stopVideos()
     }
 }
