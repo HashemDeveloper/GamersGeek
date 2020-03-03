9a692bbdc6c2efc8d15fca7c5384c5abc9b06851
Abul Hashem Jr <abul@limosys.com>
2020-03-03 12:37:06 -0500

    Sets up nav background image

--- app/src/main/java/com/project/gamersgeek/viewmodels/AllGamesPageViewModel.kt
+++ app/src/main/java/com/project/gamersgeek/viewmodels/AllGamesPageViewModel.kt
@@ -3,12 +3,12 @@ package com.project.gamersgeek.viewmodels
 import androidx.lifecycle.ViewModel
 import com.arlib.floatingsearchview.FloatingSearchView
 import com.project.gamersgeek.data.IGamerGeekRepository
-import com.project.gamersgeek.data.remote.IRawgGameDbApiHelper
+import com.project.gamersgeek.data.local.IGameResultDao
 import com.project.gamersgeek.events.HamburgerEvent
+import com.project.gamersgeek.models.games.Results
+import com.project.gamersgeek.models.platforms.PlatformDetails
 import com.project.neardoc.rxeventbus.IRxEventBus
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.*
 import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 
