9f39db000f48e0e1ddd198e79bedbc0b77f650b4
Abul Hashem Jr <abul@limosys.com>
2020-03-10 16:15:41 -0400

    Displays game details and plays stand alone video

--- app/src/main/java/com/project/gamersgeek/views/GameDetailsPage.kt
+++ app/src/main/java/com/project/gamersgeek/views/GameDetailsPage.kt
@@ -2,16 +2,24 @@ package com.project.gamersgeek.views
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.project.gamersgeek.R
+import com.project.gamersgeek.di.Injectable
+import com.project.gamersgeek.models.games.Results
+import com.project.gamersgeek.views.GameDetailsPageArgs.fromBundle
 import dagger.android.support.AndroidSupportInjection
+import kotlinx.android.synthetic.main.fragment_game_details_page.*
+import timber.log.Timber
 
-class GameDetailsPage : Fragment() {
-
+class GameDetailsPage : Fragment(), Injectable {
+    private val gameDetails by lazy {
+        fromBundle(arguments!!).gameDetailsPage
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,5 +28,17 @@ class GameDetailsPage : Fragment() {
         return inflater.inflate(R.layout.fragment_game_details_page, container, false)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val gameData: Results? = gameDetails
+        var videoUrl: String? = null
+        gameData?.let {result ->
+            result.videoClip.let {videoClip ->
+                videoUrl = videoClip.clip
+            }
+        }
+        videoUrl?.let {url ->
+            fragment_video_player_view_id.setSource(url)
+        }
+    }
 }
